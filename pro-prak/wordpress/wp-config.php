cb1c934d840991c4af95c692f4b7b17a26187794
Thomas Happé <333893@student.mboutrecht.nl>
2022-05-10 15:06:22 +0200

    wordpress config

--- pro-prak/wordpress/wp-config.php
+++ pro-prak/wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'project-wp' );
 
 /** Database username */
-define( 'DB_USER', 'user' );
+define( 'DB_USER', 'project' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'thomas' );
+define( 'DB_PASSWORD', 'project' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
